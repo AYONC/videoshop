05660b8514949e554d9b66f1876583695d0f6204
김정근 <snorlax@ridi.com>
2017-11-02 19:31:00 +0900

    elasticache dev용 작성

--- src/main/kotlin/com/ridi/config/RedisConfig.kt
+++ src/main/kotlin/com/ridi/config/RedisConfig.kt
@@ -3,9 +3,11 @@ package com.ridi.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.session.data.redis.config.ConfigureRedisAction
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 
 
@@ -32,4 +34,8 @@ class RedisConfig {
         redisTemplate.connectionFactory = connectionFactory()
         return redisTemplate
     }
+
+    @Bean
+    @Profile("dev")
+    fun configureRedisAction(): ConfigureRedisAction = ConfigureRedisAction.NO_OP
 }
