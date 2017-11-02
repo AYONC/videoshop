cdf96279a0a4a40d0624c07c23c26e2aab49a6f1
김정근 <snorlax@ridi.com>
2017-11-02 17:29:14 +0900

    redis셋팅추가 & session 테스트코드

--- src/main/kotlin/com/ridi/domain/home/HomeController.kt
+++ src/main/kotlin/com/ridi/domain/home/HomeController.kt
@@ -6,6 +6,11 @@ import com.ridi.domain.task.dto.TaskSummaryResponse
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.servlet.ModelAndView
+import javax.servlet.http.HttpSession
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+
 
 @Controller
 class HomeController(
@@ -17,4 +22,11 @@ class HomeController(
         "task_summaries" to taskService.findAll().map { TaskSummaryResponse(it) },
         "members" to memberService.findAll()
     ))
+
+    @ResponseBody
+    @RequestMapping(path = arrayOf("/session-test"), produces = arrayOf("text/plain"))
+    fun sessionTest(session: HttpSession): String {
+        session.setAttribute("test", "hello")
+        return session.getAttribute("test") as String
+    }
 }
