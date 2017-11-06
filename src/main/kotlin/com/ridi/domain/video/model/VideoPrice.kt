95566941dc5cdd9f537fc30c6dc0b6a7aacadf18
김정근 <snorlax@ridi.com>
2017-11-06 15:08:09 +0900

    Video Price starteAt필드추가

--- src/main/kotlin/com/ridi/domain/video/model/VideoPrice.kt
+++ src/main/kotlin/com/ridi/domain/video/model/VideoPrice.kt
@@ -11,5 +11,6 @@ data class VideoPrice(
         @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long = 0,
         @Column @NotNull val price: Int = 0,
         @JoinColumn(name = "video_id") @ManyToOne(targetEntity = Video::class) @NotNull val video: Video,
+        @Column(name = "started_at") @NotNull val startedAt: Date = Date(),
         @Column(name = "created_at") @NotNull val createdAt: Date = Date()
 )
