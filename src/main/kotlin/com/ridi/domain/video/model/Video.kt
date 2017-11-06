4352e68f313da5b9c6c72628307674b557a623af
김정근 <snorlax@ridi.com>
2017-11-06 14:50:22 +0900

    video model추가

---
+++ src/main/kotlin/com/ridi/domain/video/model/Video.kt
@@ -0,0 +1,16 @@
+package com.ridi.domain.video.model
+
+import java.util.*
+import javax.persistence.*
+import javax.validation.constraints.NotNull
+
+
+@Entity
+@Table(name = "video")
+data class Video(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long = 0,
+    @Column @NotNull val title: String,
+    @Column @NotNull val description: String,
+    @Column(name = "cover_path", nullable = true) val coverPath: String,
+    @Column(name = "created_at") @NotNull val createdAt: Date = Date()
+)
