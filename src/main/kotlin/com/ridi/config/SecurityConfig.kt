ea60804a21b917e41c3abf965b366a32a347668c
최아연 <ayon.c@ridi.com>
2017-12-01 11:13:44 +0900

    코드 형태 변경

--- src/main/kotlin/com/ridi/config/SecurityConfig.kt
+++ src/main/kotlin/com/ridi/config/SecurityConfig.kt
@@ -31,34 +31,37 @@ class SecurityConfig(
     }
 
     override fun configure(http: HttpSecurity) {
-        // 권한별 접근 제어를 설정
-        // permitAll() 로그인 하지 않은 사용자 포함 모두 허용
-        // .antMatchers("/admin/**/").hasIpAddress()
-        // .hasIpAddress() 특정 ip만 접근 가능하도록 설정
-        // .hasRole .hasAnyRole 로 url 접근 권한 설정 가능
-        http
-            .authorizeRequests()
-            .antMatchers("/account/login", "/account/registrationConform").permitAll()
-            .antMatchers("/account/logout").permitAll()
-            .antMatchers("/account/staff/register").permitAll()
-            .antMatchers("/account/customer/register").permitAll()
-            .antMatchers("/**").authenticated()
-            .anyRequest().authenticated() // 여기 설정된 이외의 모든 리퀘스트는 로그인 사용자에게만 허용
-            .and()
-
-            .formLogin()
-            .loginProcessingUrl("/account/login")
-            .loginPage("/account/login")
-            .failureUrl("/account/login?error=true")
-            .successHandler(authenticationSuccessHandler)
-            .failureHandler(authenticationFailureHandler)
-            .authenticationDetailsSource(authenticationDetailsSource)
-            .and()
-
-            .logout()
-            .logoutRequestMatcher(AntPathRequestMatcher("/logout"))
-            .logoutSuccessUrl("/account/login")
-            .permitAll()
+        // Access
+        http.authorizeRequests().apply {
+            antMatchers("/account/login", "/account/registrationConform").permitAll()
+            antMatchers("/account/logout").permitAll()
+            antMatchers("/account/staff/register").permitAll()
+            antMatchers("/account/customer/register").permitAll()
+            antMatchers("/**").authenticated()
+            anyRequest().authenticated() // 여기 설정된 이외의 모든 리퀘스트는 로그인 사용자에게만 허용
+            and()
+            // fixme: 권한별 접근 제어를 설정
+            // permitAll() 로그인 하지 않은 사용자 포함 모두 허용
+            // .antMatchers("/admin/**/").hasIpAddress()
+            // .hasIpAddress() 특정 ip만 접근 가능하도록 설정
+            // .hasRole .hasAnyRole 로 url 접근 권한 설정 가능
+        }
+        // Login
+        http.formLogin().apply {
+            loginProcessingUrl("/account/login")
+            loginPage("/account/login")
+            failureUrl("/account/login?error=true")
+            successHandler(authenticationSuccessHandler)
+            failureHandler(authenticationFailureHandler)
+            authenticationDetailsSource(authenticationDetailsSource)
+            and()
+        }
+        // Logout
+        http.logout().apply {
+            logoutRequestMatcher(AntPathRequestMatcher("/logout"))
+            logoutSuccessUrl("/account/login")
+            permitAll()
+        }
     }
 
     @Bean
